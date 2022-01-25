72e67a3649d5b3945a8e25b5b65594a82728e3c6
Estrela Steinkirch <estrela.steinkirch@gmail.com>
2022-01-25 18:16:32 +0100

    day 2 - hooks and pages

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,8 @@
-import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import MyApp from "./_app";
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 function Title(props) {
   const Tag = props.tag || h1;
@@ -61,11 +33,11 @@ function Title(props) {
 // export default HomePage;
 
 export default function PaginaInicial() {
-  const username = "peas";
+  const [username, setUsername] = useState("estrelasteinkirch");
+  const history = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -95,6 +67,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(event) => {
+              event.preventDefault();
+              history.push("/chat");
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -118,6 +94,8 @@ export default function PaginaInicial() {
 
             <TextField
               fullWidth
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.font[100],
